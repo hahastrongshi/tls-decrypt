1d7a33848ee9bb058bd2a4425971227fa0d80fb3
shiguoqiang <sgq1148376413@163.com>
2024-03-11 22:06:54 +0800

    feat: add gcm_128_sha256 decrypt

--- src/decrypt.rs
+++ src/decrypt.rs
@@ -1,40 +1,90 @@
-
 use openssl::symm::{Cipher, Crypter, Mode};
+use ring::aead;
 
 pub trait Decryptor {
     // the return value maybe change to another type
     fn decrypt(&self, data: &[u8]) -> Vec<u8>;
 }
 
-
 pub struct AesCbc128Sha256Decryptor {
     key: Vec<u8>,
     iv: Vec<u8>,
+    cipher: Cipher,
 }
-
 
 // todo 将加密算法独立出来，作为参数传进来，方便后续的统一处理
 impl AesCbc128Sha256Decryptor {
     pub fn new(key: Vec<u8>, iv: Vec<u8>) -> Self {
-        AesCbc128Sha256Decryptor { key, iv }
+        let cipher = Cipher::aes_128_cbc();
+        AesCbc128Sha256Decryptor { key, iv, cipher }
     }
 }
 
 impl Decryptor for AesCbc128Sha256Decryptor {
     fn decrypt(&self, data: &[u8]) -> Vec<u8> {
-        let cipher = Cipher::aes_128_cbc();
+        //let cipher = Cipher::aes_128_cbc();
 
-        let bingding = data[0..cipher.block_size()].to_vec();
-        let iv= bingding.as_ref();
-        let data = &data[cipher.block_size()..];
-        let mut decrypter = Crypter::new(cipher, Mode::Decrypt, &*self.key, Some(iv)).unwrap();
+        let bingding = data[0..self.cipher.block_size()].to_vec();
+        let iv = bingding.as_ref();
+        let data = &data[self.cipher.block_size()..];
+        let mut decrypter = Crypter::new(self.cipher, Mode::Decrypt, &*self.key, Some(iv)).unwrap();
 
-        let mut decrypted_data = vec![0; data.len() + cipher.block_size()];
+        let mut decrypted_data = vec![0; data.len() + self.cipher.block_size()];
         let mut count = decrypter.update(data, &mut decrypted_data).unwrap();
 
         count += decrypter.finalize(&mut decrypted_data[count..]).unwrap();
         decrypted_data.truncate(count);
 
-        return decrypted_data
+        return decrypted_data;
+    }
+}
+
+
+pub struct AesGCM128Sha256Decryptor {
+    key: Vec<u8>,
+    iv: Vec<u8>,
+    less_safe_key: aead::LessSafeKey,
+}
+
+impl AesGCM128Sha256Decryptor {
+    pub fn new(key: Vec<u8>, iv: Vec<u8>) -> Self {
+        // 将密钥转换为 `ring` 库的密钥类型
+        let unbound_key = aead::UnboundKey::new(&aead::AES_128_GCM, &key).unwrap();
+
+        // 将 `UnboundKey` 转换为 `LessSafeKey`
+        let less_safe_key = aead::LessSafeKey::new(unbound_key);
+        AesGCM128Sha256Decryptor { key, iv, less_safe_key }
+    }
+}
+
+impl Decryptor for AesGCM128Sha256Decryptor {
+    fn decrypt(&self, data: &[u8]) -> Vec<u8> {
+
+        let nonce_len = 8;
+        // 从 encrypted_data 中提取 nonce、
+        // 在 nonce 前面添加 1, 138, 209, 110
+        let nonce_new = &data[..nonce_len];
+        let nonce = [ &self.iv, nonce_new].concat();
+
+        // 将 Nonce 转换为 `ring` 库的 Nonce 类型
+        let nonce = aead::Nonce::try_assume_unique_for_key(&nonce).unwrap();
+
+        let mut encrypted_data = Vec::from(&data[nonce_len..]);
+
+        let additional_data = [0, 0, 0, 0, 0, 0, 0, 1, 17, 3, 3];
+
+        // 将 additional_data 扩展为包含数据长度的信息
+        let mut final_additional_data = Vec::from(additional_data);
+
+        let tag_size = aead::AES_128_GCM.tag_len();
+        let data_len = encrypted_data.len() - tag_size;
+        final_additional_data.push((data_len >> 8) as u8);
+        final_additional_data.push((data_len & 0xff) as u8);
+
+        // 解密数据
+        let decrypted_data = self.less_safe_key.open_in_place(nonce, aead::Aad::from(final_additional_data), &mut encrypted_data).unwrap();
+
+        // 返回解密结果
+        decrypted_data.to_vec()
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,51 @@ pub fn add(left: usize, right: usize) -> usize {
 
 #[cfg(test)]
 mod tests {
+    use crate::decrypt::Decryptor;
     use super::*;
+
 
     #[test]
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn gcm_128_sha256() {
+        let client_write_iv_str = b"018ad16e";
+        let client_write_key_str = b"7dc3cebbcfdd17dce17bf30dbb6cba83";
+
+        let client_write_iv = hex::decode(client_write_iv_str).unwrap();
+        let client_write_key = hex::decode(client_write_key_str).unwrap();
+
+        let encode_data_str = "0000000000000001af0cf892a0a55bfc2c0256045a9a543d86211cfe885d08195c205b0db50c07f94af7c10d38f18977607c62d231324c3a157ac91a71e64ea48c31f2967906d9e38ab1c210c5115f47f4d94341f46f5c6a928ba155d449bbc3a8c7efc2175773da699860a6d5df14b20a3afabb998b0d8f57358134ecac34";
+        let encode_data = match hex::decode(encode_data_str) {
+            Ok(data) => data,
+            Err(e) => {
+                eprintln!("解码时出错: {}", e);
+                return;
+            }
+        };
+
+        let gcm_128_sha256_decryptor = decrypt::AesGCM128Sha256Decryptor::new(client_write_key.clone(), client_write_iv.clone());
+        let decrypt_data = gcm_128_sha256_decryptor.decrypt(&*encode_data.clone());
+        println!("解密后的数据: {:?}", decrypt_data);
+
+        let newline = b"\r\n";
+        // 将 decrypt_data 按照 newline 进行切割
+        let mut result = Vec::new();
+        let mut start = 0;
+        for (i, &item) in decrypt_data.iter().enumerate() {
+            if item == newline[0] {
+                if decrypt_data[i + 1] == newline[1] {
+                    result.push(&decrypt_data[start..i]);
+                    // 将 截取 的数据转换为 utf8 格式输出
+                    let s = std::str::from_utf8(&decrypt_data[start..i]).unwrap();
+                    println!("s: {}", s);
+                    start = i + 2;
+                }
+            }
+        }
     }
 }
